8f837db0d2087b1d096e96425ea0a843b7cb26b4
Chris <chris.chu.yc@gmail.com>
2019-04-06 16:29:49 -0700

    Create views and controller operations for parts

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -1,2 +1,60 @@
 class PartsController < ApplicationController
+  def index
+    @project = Project.find(params[:project_id])
+    @parts = @project.parts.all
+  end
+
+  def show
+    @part = Part.find(params[:id])
+    @vehicle = @part.vehicle_id
+  end
+
+  def new
+    @project = Project.find(params[:project_id])
+    @vehicles = @project.vehicles.all
+    @part = Part.new()
+  end
+
+  def create
+    @project = Project.find(params[:project_id])
+    @part = @project.parts.create(part_params)
+
+    if @part.save
+      @parts = @project.parts.all
+      render 'index'
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @part = Part.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:project_id])
+    @part = Part.find(params[:id])
+
+    if @part.update_attributes(part_params)
+      flash[:success] = "Part updated"
+      @parts = @project.parts.all
+      render 'index'
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:project_id])
+    Part.find(params[:id]).destroy
+    flash[:success] = "Part deleted"
+    @parts = @project.parts.all
+    render 'index'
+  end
+
+  private
+
+  def part_params
+    params.require(:part).permit(:name, :description, :vehicle_id, :project_id, :price)
+  end
 end
--- app/models/part.rb
+++ app/models/part.rb
@@ -1,4 +1,5 @@
 class Part < ApplicationRecord
   belongs_to :vehicle
   belongs_to :project
+  belongs_to :project
 end
