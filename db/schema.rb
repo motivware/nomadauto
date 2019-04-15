c2edd739bacaa27900a7f6fa0ed7b14593f16be8
chrischu5 <chris.chu.yc@gmail.com>
2019-04-15 13:43:18 -0700

    Add quantity & part number to Parts model, update View files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190407115648) do
+ActiveRecord::Schema.define(version: 20190415203940) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,15 @@ ActiveRecord::Schema.define(version: 20190407115648) do
     t.datetime "updated_at",     null: false
     t.index ["trackable_type", "trackable_id"], name: "index_activities_on_trackable_type_and_trackable_id", using: :btree
     t.index ["user_id"], name: "index_activities_on_user_id", using: :btree
+  end
+
+  create_table "cards", force: :cascade do |t|
+    t.integer  "list_id"
+    t.string   "name"
+    t.integer  "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_cards_on_list_id", using: :btree
   end
 
   create_table "comments", force: :cascade do |t|
@@ -114,6 +123,13 @@ ActiveRecord::Schema.define(version: 20190407115648) do
     t.index ["workorder_id"], name: "index_invoices_on_workorder_id", using: :btree
   end
 
+  create_table "lists", force: :cascade do |t|
+    t.string   "new"
+    t.integer  "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "parts", force: :cascade do |t|
     t.text     "name"
     t.text     "description"
@@ -122,6 +138,8 @@ ActiveRecord::Schema.define(version: 20190407115648) do
     t.integer  "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "quantity"
+    t.string   "part_number"
     t.index ["project_id"], name: "index_parts_on_project_id", using: :btree
     t.index ["vehicle_id"], name: "index_parts_on_vehicle_id", using: :btree
   end
@@ -212,11 +230,11 @@ ActiveRecord::Schema.define(version: 20190407115648) do
     t.integer  "project_id"
     t.integer  "time"
     t.integer  "charge"
-    t.integer  "miles"
     t.index ["project_id"], name: "index_workorders_on_project_id", using: :btree
   end
 
   add_foreign_key "activities", "users"
+  add_foreign_key "cards", "lists"
   add_foreign_key "comments", "workorders"
   add_foreign_key "comments", "workorders", column: "task_id"
   add_foreign_key "customers", "projects"
