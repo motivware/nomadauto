80cb7ddc021ebad5cc6616fa3f2a46c0840ef6ea
Damon Clark <dclark312@gmail.com>
2019-06-11 09:40:26 -0400

    Invoice changes

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -20,30 +20,19 @@ class InvoicesController < ApplicationController
     #needs to be dynamic based off customer selected
     @vehicles = @project.vehicles.pluck(:make, :id)
     @workorders = @project.workorders.pluck(:title, :id)
-
-
     #authorize @invoice
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @invoice }
-    end
-
   end
 
   def create
     @project = Project.find(params[:project_id])
     @invoice = @project.invoices.create(invoice_params)
 
-    respond_to do |format|
       if @invoice.save
         #1st argument of redirect_to is an array, in order to build the correct route to the nested resource workorder
         format.html { redirect_to project_invoices_path(@project), :notice => 'invoice was successfully created.' }
         #the key :location is associated to an array in order to build the correct route to the nested resource workorder
         format.xml  { render :xml => @invoice, :status => :created, :location => [@invoice.project, @invoice] }
-
         #track_activity @invoice
-
       else
         format.html { render :action => "new" }
         format.xml  { render :xml => @invoice.errors, :status => :unprocessable_entity }
@@ -57,6 +46,6 @@ class InvoicesController < ApplicationController
     params.require(:invoice).permit(:date, :subtotal, :otherfees,
                                     :salestax, :total, :paid, :due,
                                     :workorder_id, :customer_id,
-                                    :vehicle_id)
+                                    :vehicle_id, :parts_id)
   end
 end
