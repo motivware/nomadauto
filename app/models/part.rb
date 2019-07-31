8593c4a533dbec011beb5bef6551c35750e09b38
Chris <chris.chu.yc@gmail.com>
2019-07-30 23:53:19 -0700

    Create join table for invoice to add multiple parts

--- app/models/part.rb
+++ app/models/part.rb
@@ -2,7 +2,7 @@ class Part < ApplicationRecord
   belongs_to :vehicle
   belongs_to :project
 
-  has_many :parts_to_invoices
+  has_many :parts_to_invoices, dependent: :destroy
 
   has_one :invoice
 end
--- db/migrate/20190730065333_create_parts_to_invoices.rb
+++ db/migrate/20190730065333_create_parts_to_invoices.rb
@@ -1,8 +1,8 @@
 class CreatePartsToInvoices < ActiveRecord::Migration[5.0]
   def change
     create_table :parts_to_invoices do |t|
-      t.references :invoice, foreign_key: true
-      t.references :part, foreign_key: true
+      t.references :invoice, foreign_key: true, on_delete: :cascade
+      t.references :part, foreign_key: true, on_delete: :cascade
 
       t.timestamps
     end
