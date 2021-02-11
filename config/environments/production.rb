7221b38a63a30f3eb608e35f94b8d27d2684de4e
dclark312 <dclark312@gmail.com>
2021-02-11 17:30:20 -0500

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,5 +103,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.middleware.use SetCookieDomain, .motivware.com
+  config.middleware.use SetCookieDomain, ".motivware.com"
 end
