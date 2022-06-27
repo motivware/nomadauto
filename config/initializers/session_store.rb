6f23e01331f27b1b77b07baa3a88e44dfa0a7d95
Damon Clark <dclark312@gmail.com>
2022-06-27 11:55:50 -0400

    update session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,7 +3,7 @@
 # Be sure to restart your server when you modify this file.
 
 if Rails.env.production?
-  Rails.application.config.session_store :cookie_store, key: '_saasapp_session', domain: '.nomadagile.com'
+  Rails.application.config.session_store :cookie_store, key: '_saasapp_session', domain: '.motivware.com'
 else
   Rails.application.config.session_store :cookie_store, key: '_saasapp_session', domain: '.lvh.me'
 end
