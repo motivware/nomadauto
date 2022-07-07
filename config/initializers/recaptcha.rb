56219ebec39fdb29e0e4c9c19e5b50cc9082ea87
Damon Clark <dclark312@gmail.com>
2022-07-06 21:58:21 -0400

    update-recaptcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.public_key  = '6LfIfc4gAAAAACLKD_mGmKBnM_5281m5HqiwYrAg'
-  config.private_key = '6LfIfc4gAAAAANKxFK8VhpPcCyjCt1fiTkxK9cxm'
+  config.site_key  = '6LfIfc4gAAAAACLKD_mGmKBnM_5281m5HqiwYrREAg'
+  config.secret_key = '6LfIfc4gAAAAANKxFK8VhpPcCyjCt1fiTkxK9cxm'
 end
