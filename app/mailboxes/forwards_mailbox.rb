c308fd56a4274751aeed634dedd049b163dda1cb
Damon Clark <dclark312@gmail.com>
2022-10-16 22:25:59 -0400

    try again

--- app/mailboxes/forwards_mailbox.rb
+++ app/mailboxes/forwards_mailbox.rb
@@ -7,7 +7,7 @@ class ForwardsMailbox < ApplicationMailbox
     Ticket.create({
       subject: mail.subject,
       from: mail.from,
-      details: mail.text,
+      details: mail.body.text,
       project_id: project_id.id
     })
   end
