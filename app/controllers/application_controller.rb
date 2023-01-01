38d10f32573b74d109fe09b7960ae3400ead9b5e
Damon Clark <dclark312@gmail.com>
2023-01-01 00:59:41 -0500

    update logout session redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -81,7 +81,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_out_path_for(_resource_or_scope)
-    redirect_to logout_path(subdomain: request.subdomain)
+    redirect_to logout_path(subdomain: "")
   end
 
   def after_invite_path_for(_resource_or_scope)
