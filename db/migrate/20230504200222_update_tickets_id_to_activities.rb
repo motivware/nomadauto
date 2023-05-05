f27b6f6c4a2991126692027b46dcdb2e8512e5a6
Damon Clark <dclark312@gmail.com>
2023-05-05 15:30:03 -0400

    update

--- db/migrate/20230504200222_update_tickets_id_to_activities.rb
+++
@@ -1,6 +0,0 @@
-class UpdateTicketsIdToActivities < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :activities, :ticket, foreign_key: true
-    remove_reference :activities, :tickets, foreign_key: true
-  end
-end
