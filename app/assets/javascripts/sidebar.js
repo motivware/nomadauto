2b174abb9de40c0fb5fdd7524e35fda8aceb2e27
Damon Clark <dclark312@gmail.com>
2023-05-14 21:13:09 -0400

    fix mobile nav icon

--- app/assets/javascripts/sidebar.js
+++ app/assets/javascripts/sidebar.js
@@ -1,9 +1,4 @@
-$(function(){
-  if ( $(window).width() > 767) {
-    //Add your javascript for large screens here
-    $("#menu-toggle").click(function(e) {
-      e.preventDefault();
-      $("#wrapper").toggleClass("active");
-    });
-  }
-});
+import { initFlowbite } from 'flowbite'
+useEffect(() => {
+    initFlowbite();
+  }, []);
