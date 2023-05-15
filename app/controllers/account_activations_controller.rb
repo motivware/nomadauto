36da7859752b7bdbf86c6f81c813cb9b35b6b20a
Damon Clark <dclark312@gmail.com>
2023-05-15 00:41:44 -0400

    remove boring activation landing page

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -8,7 +8,14 @@ class AccountActivationsController < ApplicationController
       log_in user
       user.save
       flash[:success] = 'Account activated!'
-      redirect_to projects_url(subdomain: user.subdomain)
+      if user.plan_id == 1 || user.plan_id == 2
+        Project.create(title: user.subdomain, details: "Sample project", user_id: user.id)
+        @project = Project.find_by(user_id: user.id)
+      	redirect_to project_contacts_url(subdomain: user.subdomain, project_id: @project)
+      else user.plan_id == 3
+        @project = Project.find_by(title: user.subdomain)
+      	redirect_to project_contacts_url(subdomain: user.subdomain, project_id: @project)
+      end
     else
       flash[:danger] = 'Invalid activation link'
       redirect_to root_url
