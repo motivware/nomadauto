28dee9fa01af49d79317d9c7ae832871e82a1cd2
dclark312 <dclark312@gmail.com>
2026-02-13 12:33:41 -0500

    Make contacts migration resilient to existing schema

--- db/migrate/20231008103215_update_contacts.rb
+++ db/migrate/20231008103215_update_contacts.rb
@@ -1,11 +1,12 @@
 class UpdateContacts < ActiveRecord::Migration[7.0]
   def change
-    remove_column :contacts, :company
-    remove_column :contacts, :website
-    add_column :contacts, :start_date, :date
-    add_column :contacts, :end_date, :date
-    add_column :contacts, :pay_rate, :integer
-    add_column :contacts, :status, :string
-    add_column :contacts, :notes, :text
+    remove_column :contacts, :company, if_exists: true
+    remove_column :contacts, :website, if_exists: true
+
+    add_column :contacts, :start_date, :date, if_not_exists: true
+    add_column :contacts, :end_date, :date, if_not_exists: true
+    add_column :contacts, :pay_rate, :integer, if_not_exists: true
+    add_column :contacts, :status, :string, if_not_exists: true
+    add_column :contacts, :notes, :text, if_not_exists: true
   end
 end
