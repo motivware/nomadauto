89bd0077be47398dad6de8723b55425712b36cd1
dclark312 <dclark312@gmail.com>
2026-02-13 11:51:14 -0500

    Guard legacy company phone migrations when table is missing

--- db/migrate/20170429200300_change_phone_to_string.rb
+++ db/migrate/20170429200300_change_phone_to_string.rb
@@ -2,6 +2,9 @@
 
 class ChangePhoneToString < ActiveRecord::Migration[5.0]
   def change
+    return unless table_exists?(:companies)
+    return unless column_exists?(:companies, :phone_number)
+
     change_column(:companies, :phone_number, :integer)
   end
 end
